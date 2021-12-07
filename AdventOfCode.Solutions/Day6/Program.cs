484fce15f176a317217a286fe4b2d660f5e67352
Amanda Santi <amanda.santi@hcss.com>
2021-12-06 21:31:29 -0600

    Day 6

---
+++ AdventOfCode.Solutions/Day6/Program.cs
@@ -0,0 +1,61 @@
+﻿using AdventOfCode.Solutions.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Solutions.Day6
+{
+    public class Program
+    {
+        const int MaxGestationPeriod = 8;
+
+        public static void Main()
+        {
+            var input = Input.ReadAllLines("Day6");
+            var fish = input.First().Split(',').Where(s => !string.IsNullOrWhiteSpace(s)).Select(s => int.Parse(s)).ToList();
+
+            long result;
+            result = Solve(fish, 80);
+            Dump(80, result);
+
+            result = Solve(fish, 256);
+            Dump(256, result);
+        }
+
+        public static long Solve(IEnumerable<int> fish, int nDays)
+        {
+            var fishCounts = fish.GroupBy(n => n).OrderByDescending(n => n.Key).ToDictionary(n => n.Key, n => (long) n.Count());
+
+            for(int j = 0; j <= MaxGestationPeriod; j++)
+            {
+                if(!fishCounts.TryGetValue(j, out var _))
+                {
+                    fishCounts.Add(j, 0);
+                }
+            }
+
+            for(int i = 0; i < nDays; i++)
+            {
+                long lastValue = 0;
+                for(int j = MaxGestationPeriod; j>= 0; j--)
+                {
+                    var temp = fishCounts[j];
+                    fishCounts[j] = lastValue;
+                    lastValue = temp;                    
+                }
+                fishCounts[MaxGestationPeriod] = lastValue;
+                fishCounts[6] += lastValue;
+            }
+
+            return fishCounts.Values.Aggregate((a, b) => a + b);
+        }
+
+        public static void Dump(int nDays, long result)
+        {
+            Console.WriteLine($"{result} fish after {nDays} days");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ AdventOfCode.Tests/Day6Tests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode.Solutions.Day6;
+using Xunit;
+
+namespace AdventOfCode.Tests
+{
+    public class Day6Tests
+    {
+        readonly int[] _input = new int[] { 3, 4, 3, 1, 2 };
+
+        [Theory]
+        [InlineData(0, 5)]
+        [InlineData(1, 5)]
+        [InlineData(2, 6)]
+        [InlineData(3, 7)]
+        [InlineData(18, 26)]
+        [InlineData(80, 5934)]
+        [InlineData(256, 26984457539)]
+        public void ProgramSolve(int day, long expected)
+        {
+            var result = Program.Solve(_input, day);
+            Assert.Equal(expected, result);
+        }
+    }
+}
