a627cbc8d5536d6315ac66717583123773e9a67f
Amanda Santi <amanda.santi@hcss.com>
2021-12-07 19:47:59 -0600

    Day 7 problems 1  & 2

---
+++ AdventOfCode.Solutions/Day7/Program.cs
@@ -0,0 +1,47 @@
+﻿using AdventOfCode.Solutions.Tools;
+
+namespace AdventOfCode.Solutions.Day7
+{
+    public class Program
+    {
+        public delegate int FuelCalculator(int targetPosition, int crabPosition, int crabCount);
+
+        public static void Main()
+        {
+            var input = Input.ReadAllLines("Day7")
+                .First()
+                .Split(',')
+                .Where(s => !string.IsNullOrWhiteSpace(s))
+                .Select(s => int.Parse(s));
+
+            int result;
+            result = Solve(input, Problem1);
+            Console.WriteLine($"Fuel: {result}");
+            Console.WriteLine();
+
+            result = Solve(input, Problem2);
+            Console.WriteLine($"Fuel: {result}");
+            Console.WriteLine();
+        }
+
+        public static int Solve(IEnumerable<int> crabPositions, FuelCalculator calculator)
+        {
+            var crabCountsByPosition = crabPositions.GroupBy(c => c).ToDictionary(c => c.Key, c => c.Count());
+            var minPosition = crabCountsByPosition.Keys.Min();
+            var maxPosition = crabCountsByPosition.Keys.Max();
+
+            return Enumerable.Range(minPosition, maxPosition - minPosition + 1)
+                .Select(n => crabCountsByPosition.Select(kvp => calculator(n, kvp.Key, kvp.Value)).Sum())
+                .Min();
+        }
+
+        public static FuelCalculator Problem1 => (int targetPosition, int crabPosition, int crabCount) =>
+            Math.Abs(targetPosition - crabPosition) * crabCount;
+
+        public static FuelCalculator Problem2 => (int targetPosition, int crabPosition, int crabCount) =>
+        {
+            var n = Math.Abs(targetPosition - crabPosition);
+            return Convert.ToInt32((n * (n + 1) / 2.0) * crabCount);
+        };
+    }
+}
---
+++ AdventOfCode.Tests/Day7Tests.cs
@@ -0,0 +1,25 @@
+﻿using AdventOfCode.Solutions.Day7;
+using System.Collections.Generic;
+using Xunit;
+
+namespace AdventOfCode.Tests
+{
+    public class Day7Tests
+    {
+        readonly IEnumerable<int> _input = new[] { 16, 1, 2, 0, 4, 2, 7, 1, 2, 14 };
+
+        [Fact]
+        public void ProgramSolve_Problem1()
+        {
+            var fuel = Program.Solve(_input, Program.Problem1);
+            Assert.Equal(37, fuel);
+        }
+
+        [Fact]
+        public void ProgramSolve_Problem2()
+        {
+            var fuel = Program.Solve(_input, Program.Problem2);
+            Assert.Equal(168, fuel);
+        }
+    }
+}
