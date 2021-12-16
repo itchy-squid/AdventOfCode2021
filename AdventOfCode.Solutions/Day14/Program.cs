009bbffb675d593b2d5d9b7d2a534d4df75bb169
Amanda Santi <amanda.santi@hcss.com>
2021-12-15 19:29:33 -0600

    Day 14 part 2

--- AdventOfCode.Solutions/Day14/Program.cs
+++ AdventOfCode.Solutions/Day14/Program.cs
@@ -7,55 +7,73 @@ namespace AdventOfCode.Solutions.Day14
 {
     public static class Program
     {
-        public static string NoOp(LinkedList<char> polymer) => new string(polymer.ToArray());
-        public static int Length(LinkedList<char> polymer) => polymer.Count();
-        public static long MostCommonMinusLeastCommon(LinkedList<char> polymer) 
-        {
-            var charFrequencies = polymer
-                .GroupBy(ch => ch)
-                .Select(ch => new { Character = ch, Count = ch.LongCount() })
-                .OrderBy(ch => ch.Count)
-                .ToArray();
-
-            return charFrequencies[charFrequencies.Length - 1].Count - charFrequencies[0].Count;
-        }
-            
-
         public static void Main()
         {
             var input = Input.ReadAllLines("Day14");
 
-            var result1 = Solve(input, 10, MostCommonMinusLeastCommon);
+            var result1 = Solve(input, 10);
             Console.WriteLine(result1);
             Console.WriteLine();
 
-            var result2 = Solve(input, 40, MostCommonMinusLeastCommon);
+            var result2 = Solve(input, 40);
             Console.WriteLine(result2);
             Console.WriteLine();
         }
 
-        public static T Solve<T>(IEnumerable<string> lines, int steps, Func<LinkedList<char>, T> permute)
+        public static long Solve(IEnumerable<string> lines, int steps)
         {
             var polymer = Template(lines.First());
-            var rules = Rules(lines.Skip(2)).ToImmutableList();
+            var rules = Rules(lines.Skip(2));
+
+            foreach(var pattern in polymer)
+            {
+                if (!rules.TryGetValue(pattern, out var next))
+                {
+                    next = new Rule(pattern);
+                    rules.Add(pattern, next);
+                }
+
+                next.Apply(1);
+            }
+            rules.Values.ForEach(r => r.CompleteStep());
 
             for(var i = 0; i < steps; i++)
             {
-                var ruleApplications = rules.SelectMany(r => r.Match(polymer)).ToList();
-                ruleApplications.ForEach(r => r());
+                rules.Values.ForEach(r => r.BeginStep(rules));
+                rules.Values.ForEach(r => r.CompleteStep());
             }
 
-            return permute(polymer);
+            var charFrequencies = rules
+                .SelectMany(r => new[]
+                    {
+                        new { Character = r.Key[0], Count = r.Value.Count },
+                        new { Character = r.Key[1], Count = r.Value.Count }
+                    })
+                .Concat(new[]
+                {
+                    new{Character = lines.First()[0], Count = (long)1},
+                    new{Character = lines.First().Last(), Count = (long)1 }
+                })
+                .GroupBy(r => r.Character)
+                .Select(g => new { Character = g.Key, Count = g.Select(kvp => kvp.Count).Sum() })
+                .OrderBy(kvp => kvp.Count)
+                .ToArray();
+
+            return (charFrequencies[charFrequencies.Length - 1].Count - charFrequencies[0].Count) / (long)2;
         }
 
-        private static LinkedList<char> Template(string line)
+        private static IEnumerable<string> Template(string line)
         {
-            return new LinkedList<char>(line.Trim().ToCharArray());
+            return Enumerable
+                .Range(0, line.Length - 1)
+                .Select(i => line.Substring(i, 2));
         }
 
-        private static IEnumerable<Rule> Rules(IEnumerable<string> lines)
+        private static Dictionary<string, Rule> Rules(IEnumerable<string> lines)
         {
-            return lines.Select(rule => new Rule(rule.Substring(0, 2), rule[6]));
+            return lines
+                .Select(rule => new Rule(rule.Substring(0, 2), rule[6]))
+                .ToDictionary(rule => rule.Pattern);
         }
 
     }
@@ -63,31 +81,52 @@ namespace AdventOfCode.Solutions.Day14
     public class Rule
     {
         public string Pattern { get; init; }
-        public char InjectionElement { get; init; }
 
-        public Rule(string pattern, char injectionElement)
+        public string[] PatternsCreated { get; set; }
+
+        public long Count { get; private set; }
+
+        private long NextCount { get; set; }
+
+        public Rule(string pattern, char? c = null)
         {
             Pattern = pattern;
-            InjectionElement = injectionElement;
+            NextCount = 0;
+
+            PatternsCreated = c == null
+                ? new[] { pattern }
+                : new string[] {
+                    new string(new char[] { Pattern[0], c.Value }),
+                    new string(new char[] { c.Value, Pattern[1]})
+                };
         }
 
-        public IEnumerable<Action> Match(LinkedList<char> polymer)
+        public void BeginStep(Dictionary<string, Rule> rules)
         {
-            for(var curr = polymer.First;  curr != null; curr = curr!.Next)
+            PatternsCreated.ForEach(p => UpdateRule(rules, p));
+        }
+
+        private void UpdateRule(Dictionary<string, Rule> rules, string pattern)
+        {
+            if (!rules.TryGetValue(pattern, out var next))
             {
-                if (curr.Next == null) break;
-                if (curr.Value != Pattern[0] || curr.Next.Value != Pattern[1]) continue;
-                
-                yield return DelayedAddAfter(polymer, curr, InjectionElement);
+                next = new Rule(pattern);
+                next.Count = 1;
+                rules.Add(pattern, next);
             }
+
+            next.Apply(Count);
         }
 
-        private Action DelayedAddAfter(LinkedList<char> list, LinkedListNode<char> node, char element)
+        public void Apply(long tally)
         {
-            return () =>
-            {
-                list.AddAfter(node, element);
-            };
+            NextCount += tally;
+        }
+
+        public void CompleteStep()
+        {
+            Count = NextCount;
+            NextCount = 0;
         }
     }
 }
